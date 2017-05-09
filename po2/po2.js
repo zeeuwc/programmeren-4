cc4effc97cbfbdba8d0ab65918cdc797a99f8701
Corne de Zeeuw <cmlzeeuw@avans.nl>
2017-05-09 16:37:38 +0200

    4 commit

--- po2/po2.js
+++ po2/po2.js
@@ -3,26 +3,7 @@ var app = express();
 
 
 
-app.get('/', function(request, response) {
-    response.send('Hello Avans!');
-})
 
-app.get('/about', function(request, response) {
-    response.send('Written by <corné de zeeuw>');
-})
-
-app.post('/', function(request, response) {
-    response.send('Hello Avans, POST request received!');
-})
-
-app.put('/', function(request, response) {
-    response.send('Hello Avans, PUT request received!');
-})
-
-app.all('*', function(request, response) {
-    response.status(404);
-    response.send('404 - Not found');
-})
 
 app.listen(3000, function() {
     console.log('Server app is listening on port 3000');
