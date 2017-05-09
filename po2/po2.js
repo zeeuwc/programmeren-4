ae42b75190ca1f0d887825b4667e619bf8ab48fb
Corne de Zeeuw <cmlzeeuw@avans.nl>
2017-05-09 16:29:59 +0200

    3 commit

--- po2/po2.js
+++ po2/po2.js
@@ -27,3 +27,23 @@ app.all('*', function(request, response) {
 app.listen(3000, function() {
     console.log('Server app is listening on port 3000');
 })
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+})
